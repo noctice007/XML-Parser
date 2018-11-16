eba793d0f6d1b405407465361451f64d2df92672
bond0O7 <saskeko1483@gmail.com>
2018-11-16 21:24:33 +0200

    now it support attributes

--- XML_Parser/xmlparser.cpp
+++ XML_Parser/xmlparser.cpp
@@ -17,6 +17,20 @@ void XMLParser::setParent(const string &parent)
     ind = 0;
     sp.setText(v[ind]);
 }
+void XMLParser::setParent(const string &parent, const string &attrib)
+{
+    sp.setText(Text);
+    string open = "<"+parent+" "+attrib+">";
+    string close = "</"+parent+">";
+
+    this->parent = parent;
+    string text = sp.get_string_between(open, close);
+    check(text=="", "error: couldn't find '" + parent + "' parent");
+    v.clear();
+    v = sp.get_string_between_forward(open, close);
+    ind = 0;
+    sp.setText(v[ind]);
+}
 
 string XMLParser::getString(const string &tag)
 {
@@ -27,9 +41,27 @@ string XMLParser::getString(const string &tag)
     return str;
 }
 
+string XMLParser::getString(const string& tag, const string& attrib)
+{
+    string open = "<" + tag + " " + attrib + ">";
+    string close = "</" + tag + ">";
+    string str = sp.get_string_between(open, close);
+    check(str=="", "error: couldn't find '" + tag + "' tag in '" + parent + "'");
+    return str;
+}
+
 int XMLParser::getInt(const string& tag)
 {
     string t = getString(tag);
+
+    istringstream is(t);
+    int num = 0;
+    is >> num;
+    return num;
+}
+int XMLParser::getInt(const string& tag, const string& attrib)
+{
+    string t = getString(tag, attrib);
 
     istringstream is(t);
     int num = 0;
@@ -46,10 +78,28 @@ long XMLParser::getLong(const string& tag)
     is >> num;
     return num;
 }
+long XMLParser::getLong(const string& tag, const string& attrib)
+{
+    string t = getString(tag, attrib);
+
+    istringstream is(t);
+    long num = 0;
+    is >> num;
+    return num;
+}
 
 bool XMLParser::getBool(const string& tag)
 {
     string t = getString(tag);
+
+    istringstream is(t);
+    bool num = 0;
+    is >> num;
+    return num;
+}
+bool XMLParser::getBool(const string& tag, const string& attrib)
+{
+    string t = getString(tag, attrib);
 
     istringstream is(t);
     bool num = 0;
@@ -66,6 +116,15 @@ char XMLParser::getChar(const string& tag)
     is >> num;
     return num;
 }
+char XMLParser::getChar(const string& tag, const string& attrib)
+{
+    string t = getString(tag, attrib);
+
+    istringstream is(t);
+    char num = 0;
+    is >> num;
+    return num;
+}
 
 float XMLParser::getFloat(const string& tag)
 {
@@ -76,10 +135,28 @@ float XMLParser::getFloat(const string& tag)
     is >> num;
     return num;
 }
+float XMLParser::getFloat(const string& tag, const string& attrib)
+{
+    string t = getString(tag, attrib);
+
+    istringstream is(t);
+    float num = 0;
+    is >> num;
+    return num;
+}
 
 double XMLParser::getDouble(const string& tag)
 {
     string t = getString(tag);
+
+    istringstream is(t);
+    double num = 0;
+    is >> num;
+    return num;
+}
+double XMLParser::getDouble(const string& tag, const string& attrib)
+{
+    string t = getString(tag, attrib);
 
     istringstream is(t);
     double num = 0;
@@ -108,13 +185,14 @@ bool XMLParser::prev()
     return true;
 }
 
-XMLParser* XMLParser::subParent(const string& parent)
+unique_ptr<XMLParser> XMLParser::subParent(const string& parent)
 {
-    if(xp != nullptr)
-    {
-        delete xp;
-    }
-    xp = get_sub_parent(parent, v[ind]);
+    unique_ptr<XMLParser> xp = get_sub_parent(parent, v[ind]);
+    return xp;
+}
+unique_ptr<XMLParser> XMLParser::subParent(const string& parent, const string& attrib)
+{
+    unique_ptr<XMLParser> xp = get_sub_parent(parent, attrib, v[ind]);
     return xp;
 }
 }
--- XML_Parser/xmlparser.hpp
+++ XML_Parser/xmlparser.hpp
@@ -2,9 +2,14 @@
 #define XMLPARSER
 
 #include <sstream>
-#include "proccess/string_proccess.hpp"
-#include "errors.hpp"
 #include <fstream>
+#include <memory>
+
+#include "proccess/string_proccess.hpp"
+
+#include "errors.hpp"
+
+using std::unique_ptr;
 using std::ifstream;
 using std::istringstream;
 using namespace XML::proccess;
@@ -18,7 +23,7 @@ class XMLParser
     string_proccessor sp;
     string Text;
     string fileName;
-    XMLParser* xp = nullptr;
+    unique_ptr<XMLParser>xp;
     size_t ind;
 
     void check(const bool exp, const string& err_msg)
@@ -43,11 +48,34 @@ class XMLParser
         return text;
     }
     XMLParser(): sp(""){}
-    XMLParser* get_sub_parent(const string& parent, const string& text)
+
+    unique_ptr<XMLParser> get_sub_parent(const string& parent, const string& text)
     {
         string open = "<" + parent + ">";
         string close = "</" + parent + ">";
-        XMLParser* xp = new XMLParser;
+
+        string t = sp.get_string_between(open, close);
+        check(t=="", "couldn't find '" + parent + "' sub-parent");
+
+        unique_ptr<XMLParser>xp(new XMLParser);
+        xp->sp.setText(text);
+
+        xp->Text = text;
+        xp->ind = 0;
+
+        xp->v = xp->sp.get_string_between_forward(open, close);
+        xp->sp.setText(xp->v[xp->ind]);
+        xp->parent = parent;
+        return xp;
+    }
+    unique_ptr<XMLParser> get_sub_parent(const string& parent, const string& attrib,
+                              const string& text)
+    {
+        string open = "<" + parent + " " + attrib + ">";
+        string close = "</" + parent + ">";
+        string t = sp.get_string_between(open, close);
+        check(t=="", "couldn't find '" + parent + "' sub-parent");
+        unique_ptr<XMLParser>xp(new XMLParser);
         xp->sp.setText(text);
 
         xp->Text = text;
@@ -66,33 +94,48 @@ public:
         Text = load(fileName);
         setParent(parent);
     }
+    explicit XMLParser(const string& parent, const string& attrib, const string& fileName): sp(""),
+        fileName(fileName)
+    {
+        Text = load(fileName);
+        setParent(parent, attrib);
+    }
 
     void setParent(const string& parent);
+    void setParent(const string& parent, const string& attrib);
 
     string getString(const string& tag);
+    string getString(const string& tag, const string& attrib);
 
     int getInt(const string& tag);
+    int getInt(const string& tag, const string& attrib);
 
     long getLong(const string& tag);
+    long getLong(const string& tag, const string& attrib);
 
     bool getBool(const string& tag);
+    bool getBool(const string& tag, const string& attrib);
 
     char getChar(const string& tag);
+    char getChar(const string& tag, const string& attrib);
 
     double getDouble(const string& tag);
+    double getDouble(const string& tag, const string& attrib);
 
     float getFloat(const string& tag);
+    float getFloat(const string& tag, const string& attrib);
+
 
     bool next();
 
     bool prev();
 
-    XMLParser* subParent(const string& parent);
+    unique_ptr<XMLParser> subParent(const string& parent);
+    unique_ptr<XMLParser> subParent(const string& parent, const string& attrib);
 
     ~XMLParser()
     {
         v.clear();
-        if(xp != nullptr)delete xp;
     }
 };
 }
